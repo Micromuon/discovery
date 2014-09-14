efc9ba176f596d767fcd55e6c6420c3eaa317869
lottec <charlotte.cutts@bskyb.com>
2014-09-14 22:17:28 +0100

    initial commit

---
+++ discovery.js
@@ -0,0 +1,96 @@
+var databaseUrl = "discoveryDB"; 
+var collections = ["services"]
+var db = require("mongojs").connect(databaseUrl, collections);
+var assert = require("assert"),
+    NRP = require("node-redis-pubsub-fork"),
+    pubsubChannel = new NRP({ scope: "messages" });
+
+var onErr = function(err) {
+    console.log(err);
+}
+
+pubsubChannel.on("discovery:getInfo", function(data) {
+    db.open(function(err, db) {
+        if (!err) {
+            db.collection("services", function(err, services) {
+                if (!err) {
+                    services.find({"name": data.name}).toArray(function(err, servicesRes) {
+                        if (!err) {
+                            pubsubChannel.emit("discovery:serviceInfo", {"serviceName": data.name, "serviceInfo": servicesRes});
+                        } else {onErr(err);}
+                    });
+                } else {onErr(err);}
+            });
+        } else {onErr(err);}
+    });
+});
+
+pubsubChannel.on("discovery:getServices", function() {
+    db.open(function(err, db) {
+        if (!err) {
+            db.collection("services", function(err, services) {
+                if (!err) {
+                    services.find().toArray(function(err, servicesRes) {
+                        if (!err) {
+                            pubsubChannel.emit("discovery:services", {"services": servicesRes});
+                        } else {onErr(err);}
+                    });
+                } else {onErr(err);}
+            });
+        } else {onErr(err);}
+    });
+});
+
+pubsubChannel.on("deployer:deployResult", function(data) {
+    if(data.failed == null) {
+        db.services.save({"name": data.name,
+                    "url": data.url,
+                    "path": data.path,
+                    "status": data.status
+                        }, function(err, saved) {
+          if( err || !saved ) {
+              console.log("Service not saved: " + data.name);
+          } else {
+              console.log("Service saved: " + data.name);
+              pubsubChannel.emit("discovery:saved", {name: data.name});
+          }
+        });
+    }
+});
+
+pubsubChannel.on("deployer:startResult", update);
+pubsubChannel.on("deployer:stopped", update);
+
+function update(data) {
+    if(data.failed == null) {
+        db.open(function(err, db) {
+            db.collection("services").findAndModify(
+                { name: data.name },
+                [['_id','asc']],
+                { $set: { status: data.status,
+                          port: data.port,
+                          processId: data.processId
+                        } },
+                function(err, object) {
+                    if (err) {
+                        console.warn(err.message);
+                    } else {
+                        pubsubChannel.emit("discovery:updated", {"status":data.status,"name":data.name});
+                    }
+                }
+            );
+        });
+    }
+}
+
+pubsubChannel.on("deployer:deleted", function(data) {
+    db.services.remove({ name: data.name
+              }, function(err, removed) {
+      if( err || !removed ) {
+          console.log("Service not removed");
+      } else {
+          console.log("Service removed");
+          pubsubChannel.emit("discovery:removed", {message: "Service removed"});
+      }
+    });
+});
---
+++ test/testdiscovery.js
@@ -0,0 +1,140 @@
+var databaseUrl = "discoveryDB";
+var collections = ["services"]
+var db = require("mongojs").connect(databaseUrl, collections);
+var assert = require("assert"),
+    NRP = require("node-redis-pubsub-fork"),
+    pubsubChannel = new NRP({ scope: "messages" });
+
+var timers = require("timers");
+
+var postData = {"name": "testservice",
+                "url": "gitlab@git.bskyb.com:sea-microservices/microservices-testservice.git",
+                "path": "repos/sea-microservices/microservices-testservice",
+                "status": "deployed",
+                "port": "null"}
+
+require("../discovery");
+
+after(function(){
+    db.services.drop();
+});
+
+describe("test discovery: ", function() {
+
+    it("gets messages and stores them", function(done) {
+        this.timeout(10000);
+        pubsubChannel.emit("deployer:deployResult", postData);
+        pubsubChannel.once("discovery:saved", function(data) {
+            db.services.find(postData, function(err, result) {
+            if( err || !result) console.log("Incorrect service data has been saved");
+              else {
+                console.log(result);
+                done();
+              }
+            });
+        });
+    });
+
+    it("allows retrieval of service data", function(done) {
+        pubsubChannel.emit("discovery:getInfo", {"name": "testservice"});
+        pubsubChannel.on("discovery:serviceInfo", function(data) {
+            var result = data.serviceInfo;
+            for (var i = 0; i < result.length; i++) {
+                delete result[i]["_id"];
+            }
+            if ( equals([postData], result) ) {
+                console.log(data);
+                done();
+            } else {
+                console.log("Incorrect Data: " + JSON.stringify(result));
+            }
+        });
+    });
+
+    it("allows updating of a service", function(done) {
+        pubsubChannel.emit("deployer:startResult", {"name": "testservice", "status": "running", "port": "12000", "processId": "12345"});
+        timers.setTimeout(function() {
+            db.services.find({"name":"testservice"}, function(err, result) {
+                if (err || !result) {
+                    console.log("No testservice found for test use");
+                } else {
+                    console.log(result[0].status);
+                    if (result[0].status == "running") {
+                        done();
+                    } else {
+                        console.log("Status not running");
+                    }
+                }
+            });
+        }, 1000);
+    });
+
+    it("allows removal of a service", function(done) {
+      this.timeout(5000);
+      pubsubChannel.emit("deployer:deployResult", postData);
+      pubsubChannel.emit("deployer:deleted", {"name": "testservice"});
+      timers.setTimeout(function() {
+        db.services.find({"name":"testservice"}, function(err, result) {
+            if (err || result.length == 0) {
+                console.log("No testservice found, removal successful!");
+                done();
+              }
+              else {
+                console.log("fail");
+              }
+          });
+        }, 3000);
+    });
+});
+
+// From http://stackoverflow.com/questions/1068834/object-comparison-in-javascript
+function equals ( x, y ) {
+    // If both x and y are null or undefined and exactly the same
+    if ( x === y ) {
+        return true;
+    }
+
+    // If they are not strictly equal, they both need to be Objects
+    if ( ! ( x instanceof Object ) || ! ( y instanceof Object ) ) {
+        return false;
+    }
+
+    // They must have the exact same prototype chain, the closest we can do is
+    // test the constructor.
+    if ( x.constructor !== y.constructor ) {
+        return false;
+    }
+
+    for ( var p in x ) {
+        // Inherited properties were tested using x.constructor === y.constructor
+        if ( x.hasOwnProperty( p ) ) {
+            // Allows comparing x[ p ] and y[ p ] when set to undefined
+            if ( ! y.hasOwnProperty( p ) ) {
+                return false;
+            }
+
+            // If they have the same strict value or identity then they are equal
+            if ( x[ p ] === y[ p ] ) {
+                continue;
+            }
+
+            // Numbers, Strings, Functions, Booleans must be strictly equal
+            if ( typeof( x[ p ] ) !== "object" ) {
+                return false;
+            }
+
+            // Objects and Arrays must be tested recursively
+            if ( !equals( x[ p ],  y[ p ] ) ) {
+                return false;
+            }
+        }
+    }
+
+    for ( p in y ) {
+        // allows x[ p ] to be set to undefined
+        if ( y.hasOwnProperty( p ) && ! x.hasOwnProperty( p ) ) {
+            return false;
+        }
+    }
+    return true;
+}
